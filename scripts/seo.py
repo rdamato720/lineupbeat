e2dac86ff1734988c210849976b1abd50dfebb7d
rdamato720 <ralph@blueprintincome.com>
2026-08-14 21:30:12 -0400

    X conversion pixel

--- scripts/seo.py
+++ scripts/seo.py
@@ -279,8 +279,25 @@ VIEW_CONTENT = """
 })();
 </script>"""
 
+# X conversion pixel.
+#
+# Like the Reddit one, this only reports on traffic from paid campaigns and
+# does nothing for organic visits. Second third-party script setting a
+# cookie, which is what brings a consent banner closer for EU and UK
+# readers -- the Cloudflare beacon sets none, and that is why there is not
+# one today.
+X_PIXEL = """
+<!-- X conversion tracking base code -->
+<script>
+!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','reect');
+</script>
+<!-- End X conversion tracking base code -->"""
+
 # Everything, for pages that want it.
-TRACKING = ANALYTICS + "\n" + REDDIT_PIXEL + TRACKING_JS
+TRACKING = ANALYTICS + "\n" + REDDIT_PIXEL + X_PIXEL + TRACKING_JS
 SPORT = "nfl"
 
 
